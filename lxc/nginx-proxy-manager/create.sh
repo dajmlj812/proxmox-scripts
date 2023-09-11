1662745222495bbc78eed41a48e3bac083ed1db2
Derek Johnson <113520895+dajmlj812@users.noreply.github.com>
2023-09-10 22:17:41 -0500

    Update create.sh
    raw base update

--- lxc/nginx-proxy-manager/create.sh
+++ lxc/nginx-proxy-manager/create.sh
@@ -31,7 +31,8 @@ function error {
 }
 
 # Base raw github URL
-_raw_base="https://raw.githubusercontent.com/ej52/proxmox-scripts/main/lxc/nginx-proxy-manager"
+_raw_base="https://raw.githubusercontent.com/dajmlj812/proxmox-scripts/blob/main/lxc/nginx-proxy-manager"
+#https://github.com/dajmlj812/proxmox-scripts/blob/main/lxc/nginx-proxy-manager
 # Operating system
 _os_type=alpine
 _os_version=3.18
